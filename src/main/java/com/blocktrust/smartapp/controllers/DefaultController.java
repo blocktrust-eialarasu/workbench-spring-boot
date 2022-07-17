e69df3cc5993c513ee38ac2c80951d5424e7d58c
Eialarasu <vteial@gmail.com>
2022-07-17 22:15:15 +0530

    updated latest changes...

--- src/main/java/com/blocktrust/smartapp/controllers/DefaultController.java
+++ src/main/java/com/blocktrust/smartapp/controllers/DefaultController.java
@@ -1,37 +1,46 @@
 package com.blocktrust.smartapp.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.Arrays;
 import java.util.Map;
 
 @Slf4j
 @RestController
 public class DefaultController {
 
+    @Resource
+    private Environment environment;
+
+    @Value("app.name")
+    String appName;
+
     @GetMapping("/ping")
     public Map<String, String> ping() {
         return Map.of("ping", "ping pong");
     }
 
-
-    @GetMapping("/test-info-log")
-    public String testInfoLog() {
-        log.info("Test Info");
-        return "Successfully logged 'Test Info'";
+    @GetMapping("/app-info")
+    public Map<String, Object> appInfo() {
+        Map<String, Object> appInfo = Map.of(
+                "mode", Arrays.asList(environment.getActiveProfiles()).stream(),
+                "name", appName
+        );
+        return appInfo;
     }
 
-    @GetMapping("/test-debug-log")
-    public String testDebugLog() {
-        log.debug("Test Debug");
-        return "Successfully logged 'Test Debug'";
+    @GetMapping("/test-log")
+    public Map<String, Object> testLog() {
+        log.error("Test error log...");
+        log.warn("Test warn log...");
+        log.info("Test info log...");
+        log.debug("Test debug log...");
+        log.trace("Test trace log...");
+        return Map.of("message", "Successfully added test logs");
     }
-
-    @GetMapping("/test-error-log")
-    public String testErrorLog() {
-        log.error("Test Error");
-        return "Successfully logged 'Test Error'";
-    }
-
 }
