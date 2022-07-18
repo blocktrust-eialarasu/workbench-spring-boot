1b982fbc45d2c71532fd44c4ea5e9966b61784d1
Eialarasu <vteial@gmail.com>
2022-07-18 17:28:57 +0530

    updating log4j configuration

--- src/main/java/io/wybis/workbench_spring_boot/controllers/DefaultController.java
+++ src/main/java/io/wybis/workbench_spring_boot/controllers/DefaultController.java
@@ -1,4 +1,4 @@
-package com.blocktrust.smartapp.controllers;
+package io.wybis.workbench_spring_boot.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
--- src/test/java/io/wybis/workbench_spring_boot/AppTests.java
+++ src/test/java/io/wybis/workbench_spring_boot/AppTests.java
@@ -1,4 +1,4 @@
-package com.blocktrust.smartapp;
+package io.wybis.workbench_spring_boot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
